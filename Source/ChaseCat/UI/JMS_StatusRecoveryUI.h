2887be452821fdfd582110da3b8f2f4a9e5ca3d7
assortrock_c2\assort_c2 <alstn7695@gmail.com>
2024-11-21 10:46:10 +0900

    화살표 좌우눌러 떨어뜨리는 UI 완성 델리게이트 추가로 서로 연결하여 구현

---
+++ Source/ChaseCat/UI/JMS_StatusRecoveryUI.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "JMS_StatusRecoveryUI.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHASECAT_API UJMS_StatusRecoveryUI : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
