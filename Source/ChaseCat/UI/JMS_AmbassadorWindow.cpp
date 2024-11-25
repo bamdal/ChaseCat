15c5670014d76bc62a682f5d487ddbb6ee314e9f
bamdal <43441777+bamdal@users.noreply.github.com>
2024-11-25 21:33:09 +0900

    BP로 있던 함수들 전부 C++로 교체 선택지 기능 반 구현 버튼 클릭후 해당 버튼에 알맞은 상태로 연결시켜주면 끝

--- Source/ChaseCat/UI/JMS_AmbassadorWindow.cpp
+++ Source/ChaseCat/UI/JMS_AmbassadorWindow.cpp
@@ -3,6 +3,7 @@
 
 #include "JMS_AmbassadorWindow.h"
 
+#include "JMS_DialogueData.h"
 #include "ChaseCat/ChaseCatCharacter.h"
 #include "Components/Button.h"
 
@@ -16,6 +17,27 @@ void UJMS_AmbassadorWindow::NativeOnInitialized()
 {
 	Super::NativeOnInitialized();
 
+
+	// DataTable 경로 설정
+	const FSoftObjectPath DataTablePath(TEXT("/Game/BP/UI/TextData/DataTable_JMSDialogueData.DataTable_JMSDialogueData"));
+
+	// 데이터 테이블 비동기 가져오기
+	TSoftObjectPtr<UDataTable> DataTableSoftObject(DataTablePath);
+	if (DataTableSoftObject.IsValid())
+	{
+		JMSDialogueDataTable = DataTableSoftObject.Get();
+	}
+	else
+	{
+		JMSDialogueDataTable = DataTableSoftObject.LoadSynchronous();
+	}
+
+	if (JMSDialogueDataTable == nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 10.0f, FColor::Red, TEXT("Failed to load DataTable_JMSDialogueData"));
+	}
+
+
 	VerticalBox = Cast<UVerticalBox>(GetWidgetFromName(TEXT("SelectBox")));
 	if(VerticalBox == nullptr)
 	{
@@ -23,7 +45,15 @@ void UJMS_AmbassadorWindow::NativeOnInitialized()
 
 	}
 
+	TalkingNameRichTextBlock = Cast<URichTextBlock>(GetWidgetFromName(TEXT("TalkingName")));
+	if(VerticalBox == nullptr)
+	{
+		GEngine->AddOnScreenDebugMessage(-1,10.0f,FColor::Red,FString::Printf(TEXT("%s TalkingNameRichTextBlock is null"),*this->GetClass()->GetName()));
+
+	}
+
 }
+
 
 void UJMS_AmbassadorWindow::UpdateSelectBox(TArray<FText> Choices)
 {
@@ -42,6 +72,8 @@ void UJMS_AmbassadorWindow::UpdateSelectBox(TArray<FText> Choices)
 		// 버튼 생성
 		UButton* NewButton = NewObject<UButton>(this);
 		UTextBlock* ButtonText = NewObject<UTextBlock>(this);
+		// 선택지가 생겼으므로 넘기기 불가능
+		bIsDialoguing = false;
 
 		if (NewButton && ButtonText)
 		{
@@ -56,16 +88,36 @@ void UJMS_AmbassadorWindow::UpdateSelectBox(TArray<FText> Choices)
 
 			// 클릭 이벤트 동적 바인딩
 			NewButton->OnClicked.AddDynamic(this, &UJMS_AmbassadorWindow::OnSelectButtonClicked);
-
+			ButtonIndexMap.Add(NewButton,i);
 			// VerticalBox에 버튼 추가
 			VerticalBox->AddChild(NewButton);
+
+			// 버튼에 개인 인덱스 추가
+
+			
 		}
 	}
 }
 
 void UJMS_AmbassadorWindow::OnSelectButtonClicked()
 {
-		GEngine->AddOnScreenDebugMessage(-1, 5.0f,FColor::Orange, FString::Printf(TEXT("%s -> Button Click"),*this->GetName()));
+
+	// 클릭된 버튼을 매핑에서 찾기
+	for (const TPair<UButton*, int32>& Pair : ButtonIndexMap)
+	{
+		if (Pair.Key->IsPressed()) // 현재 눌린 버튼인지 확인
+		{
+			//인덱스 통해구하기
+			//NextTextName = Pair.Value;
+		}
+	}
+	
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f,FColor::Orange, FString::Printf(TEXT("%s -> Button Click"),*this->GetName()));
+
+	bIsDialoguing = true;
+
+	// 버튼의 인덱스에 맞는 NextTextName으로 바꿔줘야함 
+	WriteDialogueText(NextTextName);
 }
 
 void UJMS_AmbassadorWindow::ToggleTextView()
@@ -82,8 +134,32 @@ void UJMS_AmbassadorWindow::ToggleTextView()
 }
 
 
+void UJMS_AmbassadorWindow::WriteDialogueText(FName RowName)
+{
+	if(RowName!=NAME_None)
+	{
+		FJMS_DialogueData* Row = JMSDialogueDataTable->FindRow<FJMS_DialogueData>(RowName,TEXT("JMS_AmbassadorWindow"),true);
+		TalkingNameRichTextBlock->SetText(Row->NPCName);
+		logText = Row->DialogueText;
+		StartDialogueTyping();
+		UpdateSelectBox(Row->Choices);
+		if(Row->NextDialogueIDs.Num() > 0)
+		{
+			NextTextName = Row->NextDialogueIDs[0];
+		}
+		
+		
+	}
+	else
+	{
+		GEngine->AddOnScreenDebugMessage(-1,5.0f,FColor::Blue,FString::Printf(TEXT("%s 대화 종료"),*this->GetName()));
 
-void UJMS_AmbassadorWindow::StartDialogueTyping(float TypingSpeed)
+	}
+
+}
+
+
+void UJMS_AmbassadorWindow::StartDialogueTyping()
 {
 	DialogueTextBox->SetText(FText::GetEmpty());
 	
@@ -141,14 +217,16 @@ void UJMS_AmbassadorWindow::EndDialogueText_Implementation()
 
 }
 
+
 void UJMS_AmbassadorWindow::NextDialogueText_Implementation()
 {
+
 	if(bIsTalking)
 	{
 		// 말하고 있는 중에 다음 버튼 눌렀으므로 대화창 끝까지 한번에 출력
 		GetWorld()->GetTimerManager().ClearTimer(TypingTimerHandle);
 		SetDialogueTyping();
-		
+	
 	}
 	else
 	{
@@ -157,13 +235,33 @@ void UJMS_AmbassadorWindow::NextDialogueText_Implementation()
 			EndDialogueText();
 
 		}
+
 	}
 
+	// 완성되자마자 넘어가기 방지용 부울
+	if(bIsTalking)
+	{
+		bIsTalking = false;
+	}
+	else
+	{
+		// 다음 텍스트로 넘어가기 가능할때
+		if(bIsDialoguing)
+		{
+			WriteDialogueText(NextTextName);
+		}
+	}
+
+		
+	
+	
 
 }
 
 void UJMS_AmbassadorWindow::StartDialogueText_Implementation(FName Name)
 {
 	ToggleTextView();
+
+	
 }
 
--- Source/ChaseCat/UI/JMS_AmbassadorWindow.h
+++ Source/ChaseCat/UI/JMS_AmbassadorWindow.h
@@ -7,6 +7,8 @@
 #include "JMS_AmbassadorWindow.generated.h"
 
 
+class UButton;
+struct FJMS_DialogueData;
 class URichTextBlock;
 class UVerticalBox;
 class UVerticalBoxSlot;
@@ -20,45 +22,72 @@ class CHASECAT_API UJMS_AmbassadorWindow : public UUserWidget
 	GENERATED_BODY()
 
 public:
+
+	// 데이터 테이블
+	UPROPERTY()
+	UDataTable* JMSDialogueDataTable;
+	
+	
 	// 선택지가 생길 vertical박스
 	UPROPERTY(EditAnywhere)
 	UVerticalBox* VerticalBox;
 
+	UPROPERTY(EditAnywhere)
+	URichTextBlock* TalkingNameRichTextBlock;
+
+	UPROPERTY()
+	TMap<UButton*, int32> ButtonIndexMap;
+	
+	// 선택지 생성 함수
 	UFUNCTION(BlueprintCallable)
 	void UpdateSelectBox(TArray<FText> Choices);
 
+	// 선택지 창에서의 버튼 클릭
 	UFUNCTION()
 	void OnSelectButtonClicked();
 
 	UFUNCTION(BlueprintCallable)
 	void ToggleTextView();
 
+	UFUNCTION(BlueprintCallable)
+	void WriteDialogueText(FName RowName);
+	
+
+	// 텍스트 창 처음시작 함수
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void StartDialogueText(FName Name);
 
+	// 텍스트 창 자동완성 , 다음 텍스트 넘기기
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void NextDialogueText();
-	
+
+	// 텍스트창 종료
 	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
 	void EndDialogueText();
 
+	// 다음 텍스트 rowname저장
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	FName NextTextName = TEXT("");
 
-	// 대화 출력 창
+	// 대화 출력 창 UI
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	URichTextBlock* DialogueTextBox;
-	
+
+	// 대화 저장 장소
 	UPROPERTY(VisibleAnywhere,BLUEprintReadWrite)
 	FText logText;
 
+	// 점진적 텍스트 진행용 인덱스
 	UPROPERTY()
 	int32 CurrentTextIndex = 0;
-	
+
+	// 점진적 텍스트 속도
+	UPROPERTY(EditAnywhere,BlueprintReadWrite)
+	float TypingSpeed = 10;
 	
 	// 호출시 해당 텍스트를 한글자 한글자 말하듯이 출력되게 함
 	UFUNCTION(BlueprintCallable)
-	void StartDialogueTyping(float TypingSpeed = 10);
+	void StartDialogueTyping();
 
 	// 반복 작업함수
 	UFUNCTION()
@@ -66,12 +95,18 @@ public:
 
 	// 타이머
 	FTimerHandle TypingTimerHandle;
-	
+
+	// 타이밍중이던 대화 완성시키기
 	UFUNCTION(BlueprintCallable)
 	void SetDialogueTyping();
 
+	// true 면 말하고 있는중 false면 말이 끝남
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	bool bIsTalking;
+
+	// true 면 다음 텍스트로 넘기기 가능 false면 불가능
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	bool bIsDialoguing = true;
 
 	virtual void NativeOnInitialized() override;
 };
